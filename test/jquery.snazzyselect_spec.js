e0bbc7e8368ecc36653a1d1bf8122f686bf7bf11
Ryan Sonnek <ryan@socialcast.com>
2009-07-30 15:18:51 -0500

    cleanup tests

--- test/jquery.snazzyselect_spec.js
+++ test/jquery.snazzyselect_spec.js
@@ -9,6 +9,7 @@ Screw.Unit(function(){
       var container = $('<div />').appendTo(fixtures);
       div = $('<div />').appendTo(container).addClass('snazzy_selected');
       select = $('<select />').appendTo(container).append('<option>Everyone</option>').append("<option selected='selected' value='sean'>Sean Cashin</option>").append("<option value='ryan'>Ryan Sonnek</option>").snazzySelect();
+      snazzySelect = $('ul.snazzy_select');
     });
     it("automatically hides the select input", function(){
       expect(select.is(":hidden")).to(equal, true);
@@ -17,33 +18,33 @@ Screw.Unit(function(){
       expect(div.html().replace(/\<.*/, '')).to(equal, 'Sean Cashin');
     });
     it('automatically creates a hidden unordered list with options from select input', function() {
-      expect($('ul.snazzy_select').size()).to(equal, 1);
-      expect($('ul.snazzy_select').is(':hidden')).to(equal, true);
-      expect($('ul.snazzy_select li').size()).to(equal, 3);
+      expect(snazzySelect.size()).to(equal, 1);
+      expect(snazzySelect.is(':hidden')).to(equal, true);
+      expect(snazzySelect.find('li').size()).to(equal, 3);
     });
     it('shows the unordered list on div click', function(){
       div.click();
-      expect($('ul.snazzy_select').is(":visible")).to(equal, true);
+      expect(snazzySelect.is(":visible")).to(equal, true);
     });
     it('selects li that is clicked on, replaces div contents, and hides ul', function(){
       div.click();
-      $('ul.snazzy_select li:last').click();
-      expect($('ul.snazzy_select').is(":hidden")).to(equal, true);
+      snazzySelect.find('li:last').click();
+      expect(snazzySelect.is(":hidden")).to(equal, true);
       div.children("span").remove();
-      expect(div.text()).to(equal, $("ul.snazzy_select li:last").text());
+      expect(div.text()).to(equal, snazzySelect.find("li:last").text());
     });
     it('changes div wording to match the li clicked', function(){
       div.click();
-      $('ul.snazzy_select li:last').click();
+      snazzySelect.find('li:last').click();
       div.children('span').remove();
-      expect(div.text()).to(equal, $('ul.snazzy_select li:last').text());
+      expect(div.text()).to(equal, snazzySelect.find('li:last').text());
     })
     it('highlights li that matches div', function(){
       div.click();
-      $('ul.snazzy_select li:last').click();
+      snazzySelect.find('li:last').click();
       div.click();
-      expect($('ul.snazzy_select li:last').hasClass('snazzy_hover')).to(equal, true);
-      expect($('ul.snazzy_select li:last').hasClass('snazzy_current')).to(equal, true);
+      expect(snazzySelect.find('li:last').hasClass('snazzy_hover')).to(equal, true);
+      expect(snazzySelect.find('li:last').hasClass('snazzy_current')).to(equal, true);
     });
   });
 });
